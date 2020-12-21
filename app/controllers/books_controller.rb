0284b819a4ad515fbe3c06ffc3875ecec2918396
KengoMatsuo0529 <k.matsuo0529@gmail.com>
2020-12-21 03:50:19 +0000

    [Update]Books

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,33 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to books_path
+  end
+
+  def index
+    @book = Book.all
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+
+private
+
+  def book_params
+    params.require(:book).permit(:title, :body, :user_id)
+  end
+end
